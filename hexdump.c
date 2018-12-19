df31b7137daf053082ac2e5a043f2edfec3d20b7
Bianca Power <30207144+biancapower@users.noreply.github.com>
2018-12-19 17:40:53 +1100

    implement reading from file and try buffer overflow

--- hexdump.c
+++ hexdump.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
-// courtesy Preet
-int main(int argc, char const *argv[]) {
-    int Array1[5] = {50,66,78,91,120};
 
-    for (int i=0; i< 5 ; i++) {
-        printf("%4d %4c %4x\n", Array1[i], Array1[i], Array1[i]);
+// inspired by Preet
+int main(int argc, char const *argv[]) {
+    
+    // open a file (store file handle in target_file)
+    FILE *target_file = fopen("hexdump.c", "r");
+
+    // create somewhere to store file contents
+    char buffer[24];
+
+    // read into buffer from file
+    fread(buffer, 1, 62, target_file);
+
+    for (int i=0; i < 62; i++) {
+        printf("%4d %4c %4x\n", buffer[i], buffer[i], buffer[i]);
     }
+
+    fclose(target_file);
 
     return 0;
 }

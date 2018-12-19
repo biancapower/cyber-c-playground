62f68eeee2e1a769d02aa1c741683332bf9a6c60
Bianca Power <30207144+biancapower@users.noreply.github.com>
2018-12-19 15:34:39 +1100

    add fake_print

--- strings.c
+++ strings.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+// prints a string
+void fake_print(char *string) {
 
+    printf("%c", string[0]);
+    printf("%c", string[1]);
+}
+
+int fake_strlen(char *string) {
+
+}
 
 
 // courtesy Preet
@@ -15,11 +24,14 @@ int main(int argc, char const *argv[]) {
                              'w', 'o', 'r', 'l', 'd', '\n', 0};
 
     printf(manual_string);
+    fake_print(manual_string);
+    fake_print("nice");
+
     int x = 47;
     char *easy_string = "abcd";
     x = x+1;
     for (int i = -40; i < 60 ; i++) {
-        printf("%d %4d %4c %4x\n", i, manual_string[i], manual_string[i], manual_string[i]);
+        //printf("%d %4d %4c %4x\n", i, manual_string[i], manual_string[i], manual_string[i]);
     }
 
     return 0;
